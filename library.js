508a32a64c36837ec6e7ba7f212e292bbcf4629a
Geofrey Obara Ong'eta <53979444+obara501@users.noreply.github.com>
2020-11-10 15:45:22 +0300

    creating libary

---
+++ library.js
@@ -0,0 +1,12 @@
+function Book(title,author,pages,read){
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+    this.info = function(){
+        const Book1 = new Book('The Hobbit','J.R.R Tolkien','295 pages','not read')
+    }
+    
+}
+console.log(Book1.info())
